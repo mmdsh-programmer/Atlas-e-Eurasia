b9c5bae5c08fa6be8faaa13fccf7b72ab48f0724
Mohammad Shakeri <programmer.mmdsh@gmail.com>
2020-11-18 22:28:42 +0330

    add new features

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-$(".navbar-toggler").click(() => {
-    $(".sidenav").width(250);
+AOS.init({
+    disable: false,
+    startEvent: 'DOMContentLoaded',
+    initClassName: 'aos-init',
+    animatedClassName: 'aos-animate',
+    useClassNames: false,
+    disableMutationObserver: false,
+    debounceDelay: 50,
+    throttleDelay: 99,
 
-})
+    offset: 120,
+    delay: 0,
+    duration: 400,
+    easing: 'ease',
+    once: false,
+    mirror: false,
+    anchorPlacement: 'top-bottom',
 
-$(".closebtn").click(() => {
-    $(".sidenav").width(0);
-})
+});
